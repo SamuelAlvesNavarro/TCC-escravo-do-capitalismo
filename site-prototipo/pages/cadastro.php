9209aa412530528b0beb1e706ad9fcc768d2e4cf
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-03-27 08:56:59 -0300

    desespero

--- site-prototipo/pages/cadastro.php
+++ site-prototipo/pages/cadastro.php
@@ -1,22 +1,18 @@
+<?php
+    $pdo = new PDO('mysql:host=localhost;dbname=pi', 'root', '');
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
-</head>
-<body>
-    <form>
-        <label for="name">Nome:</label><br>
-        <input type="text" id="name" name="name"><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
-    </form>
-    <button>Entrar</button><br>
-    <a href="cadastro.php"><p>Criar conta</p></a>
-</body>
-</html>
+    $nome = $_POST["name"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $confirmarsenha = $_POST["confirmarsenha"];
+
+    $sql = "SELECT * FROM user_common WHERE email = ? ";
+
+    $prepare = $pdo->prepare($sql);
+
+    $prepare->bindParam(1, $email);
+
+    if($prepare->execute() == true){
+        
+    }
+?>
