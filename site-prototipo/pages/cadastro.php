0f88ee2466f463ef9c1c329bba84af45c735500f
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-03-27 08:16:59 -0300

    cadastro - login

---
+++ site-prototipo/pages/cadastro.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form>
+        <label for="name">Nome:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+    </form>
+    <button>Entrar</button><br>
+    <a href="page/conta.html"><p>Criar conta</p></a>
+</body>
+</html>
