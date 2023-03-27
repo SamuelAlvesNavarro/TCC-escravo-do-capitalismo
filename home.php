3d0f050a68922ba66f79065637de24a3f921eeb2
Davi1544 <davi.ana969@gmail.com>
2023-03-27 09:24:05 -0300

    ed

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f2389f6c39.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/home.css">
     <title>Home Page</title>
 </head>
@@ -12,9 +13,10 @@
     <div id="all" class="all">
         <div class="menu" id="menu">
             <div class="menudiv" id="menudiv">
-               <div class="switch-mode"></div>
+                <i id="mode" class="fa-solid fa-sun"></i>
             </div>
         </div>
+        <script type="text/javascript" src="js/home.js"></script>
         <div id="main" class="main">
             <!-- <div id="sections" class="sections">
                 <div class="section home-section">
@@ -35,7 +37,7 @@
                 <div class="section exp">
 
                 </div>
-            </div> -->
+            </div>
             <div id="bestreaders" class="bestreaders">
                 <div id="bereaders" class="bereaders">
                     <div class="betitle">Os maiores leitores do site</div>
@@ -64,7 +66,7 @@
                         </tbody>
                     </table>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </body>
