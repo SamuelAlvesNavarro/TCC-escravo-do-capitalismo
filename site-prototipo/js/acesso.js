6b6d07755793f6248302e824e43afabd9e982333
Davi1544 <davi.ana969@gmail.com>
2023-04-04 12:26:39 -0300

    E

--- site-prototipo/js/acesso.js
+++ site-prototipo/js/acesso.js
@@ -1,5 +1,16 @@
 const div_cadastro = document.getElementById("container-cadastro");
-const div_login = document.getElementById("login");
+const div_login = document.getElementById("container-login");
+const div_all = document.getElementById("all");
+
+var whichimg = parseInt(Math.random() * 10);
+
+whichimg = whichimg+1;
+
+console.log(whichimg);
+linkimg = "../img/"+whichimg+".jpg";
+var linkimg = 'url('+linkimg+')';
+div_all.style.backgroundImage = linkimg;
+
 
 function showCadastro(){
     div_cadastro.classList.toggle("container-cadastro-appear");
