84382c3b873283dda603c442196c4870efa3f0f8
Davi1544 <davi.ana969@gmail.com>
2023-04-07 13:06:31 -0300

    e

--- site-prototipo/js/acesso.js
+++ site-prototipo/js/acesso.js
@@ -1,7 +1,12 @@
 const div_cadastro = document.getElementById("container-cadastro");
 const div_login = document.getElementById("container-login");
+const div_login_n = document.getElementById("login");
+var email = document.getElementById("input-email");
+var senha = document.getElementById("input-senha");
 const div_all = document.getElementById("all");
-
+const div_after = document.getElementById("after");
+var login_submit = document.getElementById("submit-login");
+var arrow = document.getElementById("arrowthing");
 var whichimg = parseInt(Math.random() * 10);
 
 whichimg = whichimg+1;
@@ -11,11 +16,31 @@ linkimg = "../img/"+whichimg+".jpg";
 var linkimg = 'url('+linkimg+')';
 div_all.style.backgroundImage = linkimg;
 
-
+function validEmail(email){
+    return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email)
+}
 function showCadastro(){
     div_cadastro.classList.toggle("container-cadastro-appear");
     div_login.classList.toggle("container-login-disappear");
+    email.value = "";
+    senha.value = "";
+    login_bt(email.value, senha.value);
 }
 function toBlack(){
-    div_all.style.backgroundImage = 'linear-gradient(to left, #121212, black)';
+    div_all.style.backgroundImage = 'linear-gradient(to left, rgb(66, 63, 63), black)';
+}
+
+senha.addEventListener('change', () => {login_bt(email.value, senha.value)});
+email.addEventListener('change', () => {login_bt(email.value, senha.value)});
+
+function login_bt(em, se){
+
+    if(se && em && validEmail(em)){
+        login_submit.classList.add("filled-login-bt");
+        arrow.classList.add("arrowappear");
+        div_login_n.style.opacity = 1;
+    }else{
+        login_submit.classList.remove("filled-login-bt");
+        arrow.classList.remove("arrowappear");
+    }
 }
