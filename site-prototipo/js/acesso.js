9f8da0e116b913aa7e84319733b4cbb9dc20309b
Davi1544 <davi.ana969@gmail.com>
2023-04-16 11:18:01 -0300

    cabei

--- site-prototipo/js/acesso.js
+++ site-prototipo/js/acesso.js
@@ -1,3 +1,4 @@
+/* VARIABLES */
 const div_cadastro = document.getElementById("container-cadastro");
 const div_login = document.getElementById("container-login");
 const div_login_n = document.getElementById("login");
@@ -8,18 +9,37 @@ const div_all = document.getElementById("all");
 const div_after = document.getElementById("after");
 var login_submit = document.getElementById("submit-login");
 var arrow = document.getElementById("arrowthing");
+
+const submit_bt = document.getElementById("submit-cadastro-bt");
+const up_apelido = document.getElementById("signup-apelido");
+const up_nome = document.getElementById("signup-nome");
+const up_email = document.getElementById("signup-email");
+const up_senha = document.getElementById("signup-senha");
+const up_confsenha = document.getElementById("signup-confsenha");
+
+/* RANDOM IMAGES */
+
 var whichimg = parseInt(Math.random() * 10);
 
 whichimg = whichimg+1;
-
-console.log(whichimg);
 linkimg = "../img/"+whichimg+".jpg";
 var linkimg = 'url('+linkimg+')';
 div_all.style.backgroundImage = linkimg;
 
+/* FUNCTIONS */
+
 function validEmail(email){
     return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email)
 }
+
+/* CADASTRO */
+
+up_apelido.addEventListener('change', () => {changebt()});
+up_nome.addEventListener('change', () => {changebt()});
+up_email.addEventListener('change', () => {changebt()});
+up_senha.addEventListener('change', () => {changebt()});
+up_confsenha.addEventListener('change', () => {changebt()});
+
 function showCadastro(){
     div_cadastro.classList.toggle("container-cadastro-appear");
     div_login.classList.toggle("container-login-disappear");
@@ -27,10 +47,19 @@ function showCadastro(){
     senha.value = "";
     login_bt(email.value, senha.value);
 }
+function changebt(){
+    if(up_apelido.value != "" && up_nome.value != "" && up_email.value != "" && up_senha.value != "" && up_confsenha.value != ""){
+        submit_bt.classList.remove("sumir-submit-cadastro-bt");
+    }else{
+        submit_bt.classList.add("sumir-submit-cadastro-bt");
+    }
+}
+changebt();
+/* LOGIN */
+
 function toBlack(){
     div_all.style.backgroundImage = 'linear-gradient(to left, rgb(66, 63, 63), black)';
 }
-
 senha.addEventListener('change', () => {login_bt(email.value, senha.value)});
 email.addEventListener('change', () => {login_bt(email.value, senha.value)});
 
@@ -44,10 +73,10 @@ function login_bt(em, se){
         arrow.classList.remove("arrowappear");
     }
 }
-
 div_after.addEventListener("click", () => {
 
     if(login_submit.classList.contains("filled-login-bt") && arrow.classList.contains("arrowappear")){
         form_login.submit();
     }
+
 })
