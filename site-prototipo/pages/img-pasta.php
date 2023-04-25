9f1c76880c3ee3a34d2db312bc0dfc32f31ab5d5
Davi1544 <davi.ana969@gmail.com>
2023-04-24 21:49:32 -0300

    e

---
+++ site-prototipo/pages/img-pasta.php
@@ -0,0 +1,43 @@
+<?php
+    //$titulo = $_POST['titulo'];
+    $titulo = "nadakksk!";
+    $titulo = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/", '/[!@#$%^&*(),.?":{}|<>\s]/'),explode(" ","a A e E i I o O u U n N"),$titulo);
+
+    images($titulo);
+    
+    function images($titulo){
+
+        function uploadImagem($name_imagem,$pasta_destino,$nome_principal){
+
+            //Capturando os dados, e armazenando em variáveis locais, e variáveis de classe
+            $name = $_FILES[$name_imagem];
+
+            $nome_substituto = $nome_principal;
+
+            $upload_arquivo = $pasta_destino.$nome_substituto;
+            move_uploaded_file($name['tmp_name'], $upload_arquivo);
+
+        }
+
+        function gerarnomepasta($titulo, $num){
+            if(is_dir("../img-story/$titulo")){
+                if($num == 0) $titulo .= '(0)';
+
+                $nump = $num + 1;
+                $titulo = str_replace("($num)", "($nump)", $titulo);
+
+                gerarnomepasta($titulo, $nump);
+            }else{
+                mkdir('../img-story/' . $titulo, 0777, false);
+                callupload($titulo);
+            }
+        }
+        function callupload($titulo){
+            for($x = 1; $x < 11; $x++){
+                uploadImagem("imagem".$x,"../img-story/$titulo/","$titulo"."-img-".$x);
+            }
+        }
+
+        $titulo = gerarnomepasta($titulo, 0);
+    }
+?>
