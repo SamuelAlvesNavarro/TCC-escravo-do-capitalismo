774921788e7260c67b63828a05c8acf4906ecfba
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-13 15:13:56 -0300

    php

---
+++ php-separado/perguntas/perguntas.php
@@ -0,0 +1,56 @@
+<?php
+    $questao = $_POST['question'];
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $c = $_POST['c'];
+    $d = $_POST['d'];
+    $certa = $_POST['certa'];
+    $perfil = 1;
+    $id_story = 1;
+    $pdo = new PDO('mysql:host=localhost;dbname=pi', 'root', '');
+    $sql = "INSERT INTO question VALUES(NULL, '$id_story','$questao')";
+    $prepare = $pdo->prepare($sql);
+    $prepare->execute();
+    $proc = "SELECT id_question FROM question WHERE fk_id_story = '$id_story'";
+    foreach($pdo->query($proc) as $key => $value){
+        $fk_id_question = $value['$id_question'];
+    }
+
+    $sql = "INSERT INTO question_user VALUES('$fk_id_question', '$perfil')";
+    $prepare = $pdo->prepare($sql);
+    $prepare->execute();
+
+    
+    switch($certa){
+        case 'a':
+            $sql = "INSERT INTO answer VALUES(NULL, '$fk_id_question', '$a', 1)";
+            $prepare = $pdo->prepare($sql);
+            $prepare->execute();
+
+            $sql = "INSERT INTO answer VALUES(NULL, '$fk_id_question', '$b', 0)";
+            $prepare = $pdo->prepare($sql);
+            $prepare->execute();
+
+            $sql = "INSERT INTO answer VALUES(NULL, '$fk_id_question', '$c', 0)";
+            $prepare = $pdo->prepare($sql);
+            $prepare->execute();
+
+            $sql = "INSERT INTO answer VALUES(NULL, '$fk_id_question', '$d', 0)";
+            $prepare = $pdo->prepare($sql);
+            $prepare->execute();
+            
+        break;
+        case 'b':
+
+        break;
+        case 'c':
+
+        break;
+        case 'd':
+
+        break;
+    }
+
+
+    echo "<a href='responder.php'>Responda</a>";
+?>
