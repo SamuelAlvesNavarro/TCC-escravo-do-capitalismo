59e2f0e20645de4d9e8dc9880057ee5f84470429
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-16 08:30:24 -0300

    php

--- site-prototipo/pages/editar_usuario.php
+++ site-prototipo/pages/editar_usuario.php
@@ -11,7 +11,20 @@
     $senha = $_POST['senha'];
     $apelido = $_POST['apelido'];
 
-    $sql = "UPDATE user_common SET nome = '$nome', email = '$email', senha = '$senha', apelido = '$apelido' WHERE fk_id_profile = '$perfil'";
+
+    $checkEmail = "SELECT * FROM user_common WHERE email = '$emailTroca'";
+    foreach($pdo->query($checkEmail) as $key => $value){
+        echo $check = $value['email'];
+    }
+
+    if($emailTroca != $email){
+        if($check == $emailTroca){
+            header("Location:error.php");
+            exit;
+        }
+    }
+
+    $sql = "UPDATE user_common SET nome = '$nome', email = '$emailTroca', senha = '$senha', apelido = '$apelido' WHERE fk_id_profile = '$perfil'";
     $prepare = $pdo->prepare($sql);
     $prepare->execute();
 
