528294b0f3356d01183fb6390cc836dbf50186f4
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-05-23 07:23:15 -0300

    erro

--- site-prototipo/pages/cadastro.php
+++ site-prototipo/pages/cadastro.php
@@ -12,13 +12,13 @@
         $confirmarsenha = $_POST["confirmarsenha"];
 
         if($senha != $confirmarsenha){
-            echo "senha não confirmada";
+            header("Location: error.php?erro=2");
         }else{
 
             $sql = "SELECT * FROM user_common WHERE email = '$email'";
 
             if($pdo->query($sql)->rowCount() > 0){
-                echo "Error: Email existente!";
+                header("Location: error.php?erro=1");
             } else{
                 $sql = "INSERT INTO profile(foto) values('0')";
                 
