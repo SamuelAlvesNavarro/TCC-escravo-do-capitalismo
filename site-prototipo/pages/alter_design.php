7613c8c2a0a553723fcbee27ab549c5f5caa9bd3
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-23 11:02:09 -0300

    at-php

--- site-prototipo/pages/alter_design.php
+++ site-prototipo/pages/alter_design.php
@@ -1,5 +1,14 @@
 <?php
+    require 'includes/conexao.php';
+    require 'includes/returnUser.php';
+
     $gadget = $_POST['gadget'];
 
-    
+    $sql = "SELECT * FROM gadget WHERE id_gadget = '$gadget'";
+    foreach($pdo->query($sql) as $key => $value){
+        $type = $value['type'];
+        $visual = $value['in_it'];
+    }
+
+    $user = "UPDATE profile SET ";
 ?>
