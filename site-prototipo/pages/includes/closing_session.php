0b98532a38f2c58c994825c94eb5c0165e3406f7
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com[>
2023-05-29 09:39:10 -0300

    at-php

--- site-prototipo/pages/includes/closing_session.php
+++ site-prototipo/pages/includes/closing_session.php
@@ -1,11 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-    require "online.php";
-    session_destroy();
-    header("Location: ../acesso.html");
-
->>>>>>> bc23f1b84d3bd0a44986842cada1c571fcfb0c1d
     require 'online.php';
     session_unset();
     session_destroy();
