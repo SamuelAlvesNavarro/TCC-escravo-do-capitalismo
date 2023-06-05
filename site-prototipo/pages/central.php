0d7424eda8b1aa05fa53b992581943f82016c7c2
Davi1544 <davi.ana969@gmail.com>
2023-06-05 09:40:09 -0300

    e

--- site-prototipo/pages/central.php
+++ site-prototipo/pages/central.php
@@ -54,29 +54,38 @@
         </div>
     </div>
 
-    <div class="header">
-        <div class="to to-store">
-            <div class="portal" onclick="aloja()">
-                <h2>Loja</h2>
-            </div>
-        </div>
-        <div class="central-menu">
-            <div class="logo-pic">
-                <svg id="eBYGrpvxAe61" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><path d="M40.1,467.1l-11.2,9c-3.2,2.5-7.1,3.9-11.1,3.9C8,480,0,472,0,462.2L0,192C0,86,86,0,192,0s192,86,192,192v270.2c0,9.8-8,17.8-17.8,17.8-4,0-7.9-1.4-11.1-3.9l-11.2-9c-13.4-10.7-32.8-9-44.1,3.9l-30.5,35c-3.3,3.8-8.2,6-13.3,6s-9.9-2.2-13.3-6l-26.6-30.5c-12.7-14.6-35.4-14.6-48.2,0L141.3,506c-3.3,3.8-8.2,6-13.3,6s-9.9-2.2-13.3-6L84.2,471c-11.3-12.9-30.7-14.6-44.1-3.9ZM160,192c0-17.673112-14.326888-32-32-32s-32,14.326888-32,32s14.326888,32,32,32s32-14.326888,32-32Zm96,32c17.673112,0,32-14.326888,32-32s-14.326888-32-32-32-32,14.326888-32,32s14.326888,32,32,32Z" transform="matrix(.707107 0.707107-.707107 0.707107 180.411309 35.439723)"/></svg>
-            </div>
-            <div class="mode-switch">
-                <div class="piece">
-                    <i id="mode" class="far fa-sun transi"></i>
+    <div class="header-all">
+        <div class="header">
+            <div class="to to-store">
+                <div class="portal" onclick="aloja()">
+                    <h2>Loja</h2>
                 </div>
             </div>
-            <div class="user-op">
-                <i class="fa-solid fa-user"></i>
+            <div class="central-menu">
+
+                <div class="logo-pic">
+                    <svg id="eBYGrpvxAe61" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><path d="M40.1,467.1l-11.2,9c-3.2,2.5-7.1,3.9-11.1,3.9C8,480,0,472,0,462.2L0,192C0,86,86,0,192,0s192,86,192,192v270.2c0,9.8-8,17.8-17.8,17.8-4,0-7.9-1.4-11.1-3.9l-11.2-9c-13.4-10.7-32.8-9-44.1,3.9l-30.5,35c-3.3,3.8-8.2,6-13.3,6s-9.9-2.2-13.3-6l-26.6-30.5c-12.7-14.6-35.4-14.6-48.2,0L141.3,506c-3.3,3.8-8.2,6-13.3,6s-9.9-2.2-13.3-6L84.2,471c-11.3-12.9-30.7-14.6-44.1-3.9ZM160,192c0-17.673112-14.326888-32-32-32s-32,14.326888-32,32s14.326888,32,32,32s32-14.326888,32-32Zm96,32c17.673112,0,32-14.326888,32-32s-14.326888-32-32-32-32,14.326888-32,32s14.326888,32,32,32Z" transform="matrix(.707107 0.707107-.707107 0.707107 180.411309 35.439723)"/></svg>
+                </div>
+                <div class="mode-switch">
+                    <div class="piece">
+                        <i id="mode" class="far fa-sun transi"></i>
+                    </div>
+                </div>
+                <div class="user-op">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <div class="banner-title">
+                    <h1>Histórias Assombrosas<h1>
+                </div>
+            </div>
+            <div class="to to-search">
+                <div class="portal" onclick="apesquisa()">
+                    <h2>Pesquisa</h2>
+                </div>
             </div>
         </div>
-        <div class="to to-search">
-            <div class="portal" onclick="apesquisa()">
-                <h2>Pesquisa</h2>
-            </div>
+        <div id="big-one" class="banner-title">
+            <h1>Histórias Assombrosas<h1>
         </div>
     </div>
 
