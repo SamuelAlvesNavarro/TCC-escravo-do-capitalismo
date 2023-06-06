b064e65fef5c2f391d6f182d8376e6cec0ced6d7
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-06-06 08:12:57 -0300

    at-php

--- php-separado/recuperarsenha/recuperar.php
+++ php-separado/recuperarsenha/recuperar.php
@@ -12,7 +12,7 @@
             $password = $value['senha'];
             $apelido = $value['apelido'];
         }
-        $url = "https://script.google.com/macros/s/AKfycby6LrnuCEJ7cVJdk3Z3qhsUyemzRO93J7zg9Qvx4qI/dev";
+        $url = "https://script.google.com/macros/s/AKfycbxju8ZwCcBEeOJEgfQ2W5-gVtOr1RIaBNlFg2A1XkOoq4X2BuWHs5MN3bSpsI7TNgU/exec";
             $ch = curl_init($url);
             curl_setopt_array($ch, [
             CURLOPT_RETURNTRANSFER => true,
@@ -20,12 +20,17 @@
             CURLOPT_POSTFIELDS => http_build_query([
                 "recipient" => $_POST['email'],
                 "subject"   => "Não responda esse email",
-                "body"      => "Olá $apelido, não sabemos se é você tentando recuperar a senha da sua conta. Se não for, logue no site e mude sua senha, visando segurança. Se for você, aqui está: " . $password
+                "body"      => "Olá $apelido, não sabemos se é você tentando recuperar a senha da sua conta. Se não for, logue no site e mude sua senha, visando segurança. Se for você, aqui está: $password
+                De todo o modo recomendamos que você troque sua senha"
             ])
             ]);
             $result = curl_exec($ch);
-            echo $result;
-            //header("Location: acesso.html");
+            if($result){
+                echo "Mensagem enviada para seu gmail. Cheque seu Spam, pois o email pode ter caído lá<br>";
+                echo "Volte para o acesso para <a href='acesso.html'>logar</a> novamente com sua senha";
+            }else{
+                header("Location: error.php?error=18");
+            }
             
     }else{
         header("Location:error.php?error=15");
