41e59af5dc058b4c0bd2fc5cbf0fd78c77d78bcc
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-07-10 12:55:36 -0300

    at-php

--- php-separado/quarentena/quarentena.php
+++ php-separado/quarentena/quarentena.php
@@ -5,7 +5,7 @@
     $id_story = $_POST['id_story'];
 
     $quarentena = "UPDATE story SET status = 4 WHERE story = '$id_story'";
-    $prepare = $pdo->prepare($user);
+    $prepare = $pdo->prepare($quarentena);
     $prepare->execute();
 
     header("Location:correcao.php");
