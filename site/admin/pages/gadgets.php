a1a862b00c598415353d83466ef74c0edc633d2e
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-07-29 11:58:22 -0300

    at-php

--- site/admin/pages/gadgets.php
+++ site/admin/pages/gadgets.php
@@ -1,22 +1,17 @@
 <?php
     require "includes/conexao.php";
     require "includes/online.php";
+
+    
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Adicionar Gadgets</title>
-    <script src="https://kit.fontawesome.com/f2389f6c39.js" crossorigin="anonymous"></script>
+    <title>Centro de Controle de Gadgets</title>
 </head>
 <body>
-    <form action="gadgets-upload.php" method="post" enctype="multipart/form-data">
-        <label for="">Gadget</label>
-        <input type="file" name="image">
-        <button>Enviar</button>
-    </form>
-
+    
 </body>
 </html>
