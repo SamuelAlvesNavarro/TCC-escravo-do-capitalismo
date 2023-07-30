75f4f42c7217c69b8c29eb52b84b8b2a9e1339d6
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-07-30 11:04:28 -0300

    at-php

--- site/admin/pages/gadgets-upload.php
+++ site/admin/pages/gadgets-upload.php
@@ -7,22 +7,23 @@
     $type = $_POST['type'];
     
     $name_image = str_replace(" ", "", $name);
+    $name_image = $_FILES[$name_image];
     $extensao = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
 
     if($type == 0){
         $path = "background-image: url(../profile-gadgets/pc-profile/$name_image.$extensao";
         //main
-        move_uploaded_file($name['tmp_name'], "../../main/profile-gadgets/pc-profile");
+        move_uploaded_file($name_image['tmp_name'], "../../main/profile-gadgets/pc-profile/");
 
         //mod
-        move_uploaded_file($name['tmp_name'], "../../mods/profile-gadgets/pc-profile");
+        move_uploaded_file($name_image['tmp_name'], "../../mods/profile-gadgets/pc-profile/");
     }else{
-        $path = "background-image: url(../profile-gadgets/pc-profile/$name_image.$extensao";
+        $path = "background-image: url(../profile-gadgets/bc-profile/$name_image.$extensao";
         //main
-        move_uploaded_file($name['tmp_name'], "../../main/profile-gadgets/bc-profile");
+        move_uploaded_file($name_image['tmp_name'], "../../main/profile-gadgets/bc-profile/");
 
         //mod
-        move_uploaded_file($name['tmp_name'], "../../mods/profile-gadgets/bc-profile");
+        move_uploaded_file($name_image['tmp_name'], "../../mods/profile-gadgets/bc-profile/");
     }
 
     $image = "INSERT INTO gadgets values(NULL, '$type', '$preco', 1, '$path')";
