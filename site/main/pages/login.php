d1445fe563e2fb8d97e6a2deb944e8b4614dd19d
Davi1544 <davi.ana969@gmail.com>
2023-07-31 20:30:33 -0300

    e

--- site/main/pages/login.php
+++ site/main/pages/login.php
@@ -17,7 +17,7 @@
         $_SESSION['email'] = $email;
         header("Location: central.php");
     }else{
-        echo "<script>setNotif('errado');</script>";
+        $wr = 1;
     }
 
 
@@ -36,15 +36,13 @@
             localStorage.setItem("AcessoNotificacao", JSON.stringify(notType));
             window.location.href = "acesso.html";
         }
-        <?php
-            if($prepare -> rowCount() > 0){
-                require "includes/criasession.php";
-                $_SESSION['email'] = $email;
-                header("Location: central.php");
-            }else{
-                echo "setNotif('errado');";
-            }
-        ?>
     </script>
+
+    <?php
+        if($wr == 1){
+            echo "<script>setNotif('errado');</script>";
+            header("Lcoation: acesso.html");
+        }
+    ?>
 </body>
 </html>
