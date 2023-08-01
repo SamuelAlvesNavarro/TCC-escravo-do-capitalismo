adf7eabdb82d6e6d932f6668602576f2bef5ac98
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-01 09:59:32 -0300

    at-php

---
+++ php-separado/comentarios/comente.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="coment.php" method="post">
+        <label for="">Comente: </label>
+        <textarea name="coment" cols="30" rows="10"></textarea>
+        <button>Comentar</button>
+    </form>
+</body>
+</html>
