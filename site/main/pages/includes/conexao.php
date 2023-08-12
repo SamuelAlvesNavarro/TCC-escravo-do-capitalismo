2a2f95419418a34893be11193676dbd212222822
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-12 17:43:11 -0300

    a

--- site/main/pages/includes/conexao.php
+++ site/main/pages/includes/conexao.php
@@ -1,7 +1,7 @@
 <?php
     //servidor --- > $pdo = new PDO('mysql:host=localhost;dbname=id20545858_pi', 'id20545858_samuel', 'Agx3((dO5ze*n-]Y');
     //escola ---> $pdo = new PDO('mysql:host=localhost;port=3307;dbname=pi', 'root', '');
-    //casa ---> $pdo = new PDO('mysql:host=localhost;dbname=pi', 'root', '');
+    //casa ---> $pdo = new PDO('mysql:host=localhost;dbname=pi', 'root', ''); a
     
     $pdo = new PDO('mysql:host=localhost;port=3306;dbname=pi', 'root', '');
 ?>
