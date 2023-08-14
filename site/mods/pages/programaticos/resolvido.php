aee990d1a9aca0051c6b09637f3a7713bf081a58
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-14 09:08:53 -0300

    at

---
+++ site/mods/pages/programaticos/resolvido.php
@@ -0,0 +1,11 @@
+<?php
+    require "../includes/conexao.php";
+    require "../includes/online.php";
+
+    $profile = $_GET['id_report'];
+    $story = $_GET['id_report'];
+    $comment = $_GET['id_report'];
+
+    $sql = "UPDATE ";
+
+?>
