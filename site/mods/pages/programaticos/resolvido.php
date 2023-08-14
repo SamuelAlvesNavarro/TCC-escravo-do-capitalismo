85cdc77af639e7e1788d98aa1f9b49d328e15aea
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-14 09:16:53 -0300

    at-php

--- site/mods/pages/programaticos/resolvido.php
+++ site/mods/pages/programaticos/resolvido.php
@@ -6,6 +6,21 @@
     $story = $_GET['id_report'];
     $comment = $_GET['id_report'];
 
-    $sql = "UPDATE ";
+    if(!isset($profile)){
+        $sql = "UPDATE report_profile SET code = 2 WHERE id_report = $profile";
+    }
+
+    if(!isset($story)){
+        $sql = "UPDATE report_story SET code = 2 WHERE id_report = $story";
+    }
+
+    if(!isset($comment)){
+        $sql = "UPDATE report_comment SET code = 2 WHERE id_report = $comment";
+    }
+    
+    $prepare = $pdo->prepare($sql);
+    $prepare->execute();
+
+    header("Location: ../report_center.php");
 
 ?>
