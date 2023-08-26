4163bd9ac5a13d45d2d4209b515f385995e702bc
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-26 11:33:49 -0300

    at-php

--- site/admin/pages/gadgets.php
+++ site/admin/pages/gadgets.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/f2389f6c39.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css?v=1.01" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Centro de Controle de Gadgets</title>
     <style>
         .foto{
@@ -37,7 +37,7 @@
         <h2>Fotos de perfil - ATIVAS</h2>
     </div>
 
-    <table align="center" class="col-3 table table-striped border border-black border-1 p-2 mt-3">
+    <table align="center" class="table table-dark table-striped">
         <thead class="thead-dark">
             <tr>
                 <th scope="col">ID Gadget</th>
@@ -54,30 +54,32 @@
                 foreach($pdo->query($gadget) as $key => $value){
 
                     echo
-                    '<tr>
-                        <td scope="row">' 
+                    '<tr scope="row">
+                        <td>' 
                             .$value['id_gadget']. 
                         '</td>'.
 
                         '<td scope="row"><div class="foto" style="'.$value['in_it'].'"></div></td>'.
 
-                        '<td scope="row">'
+                        '<td>'
                             .$value['preco'].
                         '</td>'.
 
-                        '<td scope="row"><a href="alterar-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-info">Alterar</button></a></td>'.
-                        '<td scope="row"><a href="desativar-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-primary">Desativar</button></a></td>'.
-                        '<td scope="row"><a href="excluir-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-danger">Excluir</button></a></td>'.
+                        '<td><a href="alterar-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-info">Alterar</button></a></td>'.
+                        '<td><a href="desativar-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-primary">Desativar</button></a></td>'.
+                        '<td><a href="excluir-gadget.php?id='.$value['id_gadget'].'"><button class="btn btn-danger">Excluir</button></a></td>'.
                     '</tr>';
                 }
             ?>
         </tbody>
     </table>
 
+    <div class="container text-center mb-3 border-0">
         <h2>Fundos de perfil - ATIVOS</h2>
-
-    <table>
-        <thead>
+    </div>
+        
+    <table align="center" class="table table-dark table-striped">
+        <thead class="thead-dark">
             <tr>
                 <th scope="col">ID Gadget</th>
                 <th scope="col">Fundo</th>
@@ -87,26 +89,26 @@
                 <th scope="col">Excluir</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="table-group-divider">
             <?php
                 $gadget = 'SELECT * FROM gadget WHERE type = 1 AND g_status = 1';
                 foreach($pdo->query($gadget) as $key => $value){
 
                     echo
-                    '<tr>
-                        <td scope="row">' 
+                    '<tr scope="row">
+                        <td>' 
                             .$value['id_gadget']. 
                         '</td>'.
 
-                        '<td scope="row"><div class="fundo" style="'.$value['in_it'].'"></div></td>'.
+                        '<td><div class="fundo" style="'.$value['in_it'].'"></div></td>'.
 
-                        '<td scope="row">'
+                        '<td>'
                             .$value['preco'].
                         '</td>'.
 
-                        '<td scope="row"><a href="alterar-gadget.php?id='.$value['id_gadget'].'"><button>Alterar</button></a></td>'.
-                        '<td scope="row"><a href="desativar-gadget.php?id='.$value['id_gadget'].'"><button>Desativar</button></a></td>'.
-                        '<td scope="row"><a href="excluir-gadget.php?id='.$value['id_gadget'].'"><button>Excluir</button></a></td>'.
+                        '<td><a href="alterar-gadget.php?id='.$value['id_gadget'].'"><button>Alterar</button></a></td>'.
+                        '<td><a href="desativar-gadget.php?id='.$value['id_gadget'].'"><button>Desativar</button></a></td>'.
+                        '<td><a href="excluir-gadget.php?id='.$value['id_gadget'].'"><button>Excluir</button></a></td>'.
                     '</tr>';
                 }
             ?>
