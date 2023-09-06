c5c5056234f29fecf6cc81a93d3fc9aadf172ec4
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-09-06 12:13:02 -0300

    e

--- site/main/pages/story2.php
+++ site/main/pages/story2.php
@@ -303,6 +303,15 @@
                 ac_ = 0;
             }
 
+            // When the user scrolls the page, execute myFunction
+/*window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}*/
             if(ac_ == 1){
 
                 var pageMax = reveal.offsetHeight / 10;
