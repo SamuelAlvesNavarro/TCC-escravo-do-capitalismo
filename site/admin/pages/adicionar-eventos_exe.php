20b0d81dc1a20412dc140f02e93720cc548f3e28
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-09-19 12:19:44 -0300

    at-php

--- site/admin/pages/adicionar-eventos_exe.php
+++ site/admin/pages/adicionar-eventos_exe.php
@@ -1,5 +1,11 @@
 <?php
     require "includes/conexao.php";
     require "includes/online.php";
-    
+
+    $titulo = $_POST['titulo'];
+    $type = $_POST['type'];
+    $desc = $_POST['desc'];
+    $func = $_POST['funcao'];
+    $num = $_POST['num'];
+
 ?>
