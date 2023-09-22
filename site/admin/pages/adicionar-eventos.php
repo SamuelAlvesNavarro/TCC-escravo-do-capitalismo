6291838e62fe8d264b2a6ba7822b00da00866b8c
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-09-22 18:58:08 -0300

    at-php

--- site/admin/pages/adicionar-eventos.php
+++ site/admin/pages/adicionar-eventos.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Adicionar gadgets</title>
-    <script src="https://kit.fontawesome.com/f2389f6c39.js" crossorigin="anonymous"></script>
 </head>
 <body  data-bs-theme="dark">
     <?php
@@ -29,9 +28,7 @@
 
         <label class="form-label">Função a ser executada: </label><br>
         <input type="radio" name="funcao" onclick="openX(2)" required value="2">Dar gadget <br>
-        <input type="radio" name="funcao" onclick="openX(1)" required value="1">Dar moedas
-        <br>
-        <br>
+        <input type="radio" name="funcao" onclick="openX(1)" required value="1">Dar moedas<br><br>
         <input type="hidden" id='num' name="number" placeholder="Quantidade de Moedas" class="form-control" required>
 
         <div id="gadgets" style="display: none;">
@@ -64,5 +61,6 @@
             }
         }
     </script>
+    <script src="https://kit.fontawesome.com/f2389f6c39.js" crossorigin="anonymous"></script>
 </body>
 </html>
