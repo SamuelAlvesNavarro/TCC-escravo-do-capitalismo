3f8cafe13466024e087317530b3b7a26c7216934
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-09-23 12:41:50 -0300

    at-php

---
+++ site/main/pages/excluir-comment.php
@@ -0,0 +1,8 @@
+<?php 
+    require "includes/online.php";
+    require "includes/conexao.php";
+    require "includes/returnUser.php";
+    require "includes/enviarErro.php";
+
+    $id_comment = $_POST['id_comment'];
+?>
