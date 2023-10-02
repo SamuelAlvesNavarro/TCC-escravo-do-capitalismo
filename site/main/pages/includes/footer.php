746f84cd671ae096a0594878ae9608e14ef38fac
Davi1544 <davi.ana969@gmail.com>
2023-10-02 09:38:47 -0300

    e

--- site/main/pages/includes/footer.php
+++ site/main/pages/includes/footer.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../css/footer.css?v=1.01">
+<link rel="stylesheet" href="../css/footer.css?v=1.0123">
 <footer>
     <div class="first-row">
         <div class="column-one">
@@ -6,19 +6,19 @@
             <p><a class="path-footer" href="central.php">Central</a></p>
             <p><a class="path-footer" href="loja.php">Loja</a></p>
             <p><a class="path-footer" href="writerHub.php">Área do escritor</a></p>
+            <a href="#" class="go-all-up"><i class="fa-solid fa-chevron-up"></i></a>
         </div>
-        <div class="column-one">
+        <!--<div class="column-one">
             <h2>Pesquisa</h2>
             <form action="pesquisa.php" method="get">
                 <input type="text" required name="busca" id="">
                 <input id="submit-footer" type="submit" value="Pesquisar">
             </form>
-        </div>
+        </div>-->
         <img id="img-footer" src="../img/police-man-png.png" alt="" srcset="">
     </div>
     <hr>
     <div class="copyright">
-        <a href="#"><i class="fa-solid fa-chevron-up"></i></a>
         <i class="fa-solid fa-copyright"></i>
     </div>
 </footer>
