356cfd9cdcf3bffadd15f089a4c78fcaeabfbfdf
Davi1544 <davi.ana969@gmail.com>
2023-11-26 21:53:07 -0300

    e

--- site/mods/pages/comment.php
+++ site/mods/pages/comment.php
@@ -17,10 +17,15 @@
     <title>Comentário</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
+    <style>
+        body{
+            padding: 20px;
+        }
+    </style>
 </head>
 <body data-bs-theme="dark">
-    
+    <h1>Comentário: </h1>
+    <br>
     <p class="container text-center"><?php echo $comment; ?></p>
 </body>
 </html>
